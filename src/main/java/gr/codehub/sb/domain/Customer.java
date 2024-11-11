14e7134faa34173e9ad12407a1dd4ed938630d45
Dimitrios Paschalis <paschouale@hotmail.com>
2024-11-11 11:35:49 +0200

    First Commit

---
+++ src/main/java/gr/codehub/sb/domain/Customer.java
@@ -0,0 +1,22 @@
+package gr.codehub.sb.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.stereotype.Component;
+
+import java.sql.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Component
+public class Customer {
+    private Long id; // wrapper
+    private String name;
+    private String email;
+    private Date registrationDate;
+
+}
